87925dd7bdca7d0d254451f33cc6bbb1ad6367f4
CHRISTINA OUBKEO <COUBKEO@GMAIL.COM>
2022-12-06 21:05:34 -0700

    commit tree cb41acbc06df1eeb39851959fbca762916662415

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+    return cats
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+    return cats
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+function appendCat(){
+    let cat = [...cats, "Broom"]
+    return cat
+}
+function prependCat(){
+    let catss = ["Arnold", ...cats]
+    return catss
+}
+function removeLastCat(){
+    let ccats= cats.slice(0,cats.length-1)
+    return ccats
+}
+function removeFirstCat(){
+  let ccats = cats.slice(1)
+  return ccats
+}
